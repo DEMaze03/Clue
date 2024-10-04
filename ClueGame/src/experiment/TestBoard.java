f61ebffd51729d97a2590739b35ced2b2910bba7
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-10-04 12:49:38 -0600

    Added implementations of class functions

--- ClueGame/src/experiment/TestBoard.java
+++ ClueGame/src/experiment/TestBoard.java
@@ -1,5 +1,35 @@
 package experiment;
 
-public class TestBoard {
+import java.util.Set;
+
+public class TestBoard {	
+	private Set<TestBoardCell> targetList;
+	
+
+	public TestBoard() {
+		super();
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		int cellCol = startCell.col;
+		int cellRow = startCell.row;
+		
+		targetList.add(new TestBoardCell(cellCol + pathlength, cellRow));
+		targetList.add(new TestBoardCell(cellCol - pathlength, cellRow));
+		targetList.add(new TestBoardCell(cellCol, cellRow + pathlength));
+		targetList.add(new TestBoardCell(cellCol, cellRow - pathlength));
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		TestBoardCell b = new TestBoardCell(row, col);
+		return b;
+		
+	}
+	
+	public Set<TestBoardCell> getTargets() {
+		return targetList;
+		
+	}
+	
 
 }
