b4882dd7d432456549f4d5ba51e71b488218662f
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-10-04 12:50:04 -0600

    Changed col and row to public to be accessed in TestBoard class

--- ClueGame/src/experiment/TestBoardCell.java
+++ ClueGame/src/experiment/TestBoardCell.java
@@ -3,8 +3,8 @@ package experiment;
 import java.util.Set;
 
 public class TestBoardCell {
-	private int row;
-	private int col;
+	public int row;
+	public int col;
 	private boolean isRoom;
 	private boolean occupied;
 	private Set<TestBoardCell> adjList;
