4c925171cad5fb507994af93e4c6442d5ee0c6be
Daylon Maze <daylonmaze@gmail.com>
2024-10-04 12:22:58 -0600

    Implemented TestBoardCell class

--- ClueGame/src/experiment/TestBoardCell.java
+++ ClueGame/src/experiment/TestBoardCell.java
@@ -1,5 +1,39 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.Set;
 
+public class TestBoardCell {
+	private int row;
+	private int col;
+	private boolean isRoom;
+	private boolean occupied;
+	private Set<TestBoardCell> adjList;
+	
+	TestBoardCell(int row, int col){
+		this.row = row;
+		this.col = col;
+	}
+	
+	void addAdj(TestBoardCell cell) {
+		adjList.add(cell);
+	}
+	
+	Set<TestBoardCell> getAdjList(){
+		return adjList;
+	}
+	
+	void setRoom(boolean isRoom) {
+		this.isRoom = isRoom;
+	}
+	boolean isARoom() {
+		return isRoom;
+		//Hello
+	}
+	
+	void setOccupied(boolean isOccupied) {
+		occupied = isOccupied;
+	}
+	boolean isOccupied() {
+		return occupied;
+	}
 }
