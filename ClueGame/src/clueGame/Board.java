463c219b71ecd337912e6bb619c8013406b16573
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-10-09 12:42:42 -0600

    added function signatures and delcared variables

--- ClueGame/src/clueGame/Board.java
+++ ClueGame/src/clueGame/Board.java
@@ -1,5 +1,27 @@
 package clueGame;
 
+import java.util.Map;
+
 public class Board {
+	
+	private BoardCell[][] grid;
+	private int numRows;
+	private int numColumns;
+	private String layoutConfigFile;
+	private String setupConfigFile;
+	private Map<Character, Room> roomMap;
+	static Board theInstance;
+	
+	public void initalize() {
+		
+	}
+	
+	public void loadSetupConfig() {
+		
+	}
+	
+	public void loadLayoutConfig() {
+		
+	}
 
 }
