4e3ffb364322cddd5cbac8bbf0f788d310420bce
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-10-09 18:47:43 -0600

    implemented loadSetupConfig()

--- ClueGame/src/clueGame/Board.java
+++ ClueGame/src/clueGame/Board.java
@@ -1,14 +1,18 @@
 package clueGame;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Board {
 	private BoardCell[][] grid;
 	private int numRows;
 	private int numColumns;
 	private String layoutConfigFile;
-	private String setupConfigFile;
-	private Map<Character, Room> roomMap;
+	private static String setupConfigFile;
+	private static Map<Character, Room> roomMap = new HashMap<Character, Room>();
 	static Board theInstance = new Board();
 	
 	private Board() {
@@ -23,9 +27,35 @@ public class Board {
 	
 	public void initialize() {
 		
+		loadSetupConfig();
+		
 	}
 
-	public void loadSetupConfig() {
+	public static void loadSetupConfig() {
+		
+		File setupFile = new File(setupConfigFile);
+		try {
+			Scanner r = new Scanner(setupFile);
+		      while (r.hasNextLine()) {
+		         String data = r.nextLine();
+		         String[] l = data.split(", ");
+		         if (l[0].equals("Room")) {
+		        	 char c = l[2].charAt(0);
+		        	 String rn = l[1];
+		        	 System.out.println(c + " " + rn);
+		        	 
+		        	 Room room = new Room(rn);
+		        	 roomMap.put(c, room);
+		         }
+		         
+		     }
+		      
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+		
 		
 	}
 	
@@ -36,8 +66,25 @@ public class Board {
 
 	public void setConfigFiles(String csv, String txt) {
 		// TODO Auto-generated method stub
-		theInstance.layoutConfigFile = csv;
-		theInstance.setupConfigFile = txt;
+		theInstance.layoutConfigFile = "data/" + csv;
+		theInstance.setupConfigFile = "data/" + txt;
 		
 	}
+	
+	public Room getRoom(char c) {
+		return roomMap.get(c);
+	}
+	
+	public static void main(String[] args) {
+		Board board;
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueLayout306.csv", "ClueSetup306.txt");
+		// Initialize will load BOTH config files
+		board.initialize();
+	
+		
+	}
+
+
 }
--- ClueGame/src/clueGame/Room.java
+++ ClueGame/src/clueGame/Room.java
@@ -4,5 +4,14 @@ public class Room {
 	private String name;
 	private BoardCell centerCell;
 	private BoardCell labelCell;
+	
+	public Room(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
 
 }
