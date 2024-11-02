ab00d3b25490833e3b9c4332ce12b5bf04f34d14
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-11-02 01:00:02 -0600

    added basic tests

--- ClueGame/src/tests/GameSetupTests.java
+++ ClueGame/src/tests/GameSetupTests.java
@@ -2,6 +2,9 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
 import clueGame.Board;
 
 public class GameSetupTests {
@@ -18,10 +21,31 @@ public class GameSetupTests {
 		board.initialize();
 	}
 	
+	
 	@Test
-	public static void TestBoard() {
+	public void TestPlayers() {
+		// Test 1 human player and 5 computer
+		// Test 6 players total
+		assertEquals("Test Name", board.returnPlayer("Test Name").name);
+		assertEquals(6, board.getPlayers().size());
 		
 	}
+	
+	@Test
+	public static void TestDeck() {
+		// Test Deck size
+		// Test Weapons, people and rooms
+		// Test All Cards are Dealt
+		
+		assertEquals(21, board.getDeck().size());
+		
+	}
+	@Test
+	public static void TestBoard() {
+		// Test Players having cards dealt to them properly
+		
 	}
 	
+	
 }
+	
