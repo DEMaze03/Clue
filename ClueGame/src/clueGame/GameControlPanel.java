7da512139b99a8c3eef3970924ed9683203e1bce
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-11-10 23:03:41 -0700

    updated frame to have basic control layout

--- ClueGame/src/clueGame/GameControlPanel.java
+++ ClueGame/src/clueGame/GameControlPanel.java
@@ -1,5 +1,8 @@
 package clueGame;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class GameControlPanel extends JPanel {
@@ -14,18 +17,60 @@ public class GameControlPanel extends JPanel {
 	
 	
 	public static void main(String[] args) {
-		GameControlPanel panel = new GameControlPanel();
+		GameControlPanel gamePanel = new GameControlPanel();
 		JFrame frame = new JFrame();
-		JButton button = new JButton();
-		frame.setContentPane(panel);
-		
-		frame.setSize(750, 180);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		
-		button.setText("This is a button");
-		button.setSize(200,  200);
-		button.setVisible(true);
-		frame.add(button);
+		
+		JButton button1 = new JButton();
+		button1.setText("button1");
+		button1.setSize(200,  200);
+		button1.setVisible(true);
+		
+		JButton button2 = new JButton();
+		button2.setText("button2");
+		button2.setSize(200,  200);
+		button2.setVisible(true);
+		
+		JButton button3 = new JButton();
+		button3.setText("make  accusation");
+		button3.setSize(200,  200);
+		button3.setVisible(true);
+		
+		JButton button4 = new JButton();
+		button4.setText("NEXT");
+		button4.setSize(200,  200);
+		button4.setVisible(true);
+	
+		frame.setContentPane(gamePanel);
+		frame.setLayout(new GridLayout(2, 0));
+		JPanel twinPane = new JPanel();
+		twinPane.setLayout(new GridLayout(1,4));
+		
+		twinPane.add(button1);
+		twinPane.add(button2);
+		twinPane.add(button3);
+		twinPane.add(button4);
+		
+		////
+		JTextField guess = new JTextField();
+		JTextField guessResult = new JTextField();
+		
+		JPanel lowerGuessResult = new JPanel();
+		lowerGuessResult.setLayout(new GridLayout(1,2));
+		lowerGuessResult.add(guess);
+		lowerGuessResult.add(guessResult);
+		
+		frame.add(twinPane, BorderLayout.NORTH);
+		frame.add(lowerGuessResult, BorderLayout.EAST);
+		
+		
+
+//		twinPane.setLayout(new GridLayout(1, 2));
+		
+		frame.setSize(750, 180);  // size the frame
+		
+//		frame.add(button);
 		
 		
 		frame.setVisible(true); // make it visible
