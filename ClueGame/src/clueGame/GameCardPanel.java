8f86538647c919c311bcd70b97d247d92f1528d8
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-11-11 17:49:42 -0700

    refactored init of variables outside of constructor

--- ClueGame/src/clueGame/GameCardPanel.java
+++ ClueGame/src/clueGame/GameCardPanel.java
@@ -5,40 +5,43 @@ import java.awt.*;
 import javax.swing.*;
 
 public class GameCardPanel extends JPanel{
+	private JPanel peoplePanel = new JPanel();
+	private JPanel peopleInHand = new JPanel();
+	private JTextField peopleSeen = new JTextField();
+	private JPanel roomPanel = new JPanel();
+	private JTextField roomInHand = new JTextField();
+	private JTextField roomSeen = new JTextField();
+	private JTextField weaponInHand = new JTextField();
+	private JTextField weaponSeen = new JTextField();
+	private JPanel weaponPanel = new JPanel();
+	JLabel cardLabel = new JLabel();
+	JPanel cardPanel = new JPanel();
 	
 	public GameCardPanel() {
 		
-		JPanel peoplePanel = new JPanel();
 		peoplePanel.setSize(180, 300);
-		JTextField peopleInHand = new JTextField();
-		JTextField peopleSeen = new JTextField();
+		peopleInHand.setLayout(new GridLayout(1,1));
+		peopleInHand.add(new JTextField());
+		
 		peoplePanel.setLayout(new GridLayout(2,1));
 		peoplePanel.add(peopleInHand, BorderLayout.CENTER);
 		peoplePanel.add(peopleSeen, BorderLayout.CENTER);
 		peoplePanel.setBorder(BorderFactory.createTitledBorder("Known People"));
 		
 		
-		JPanel roomPanel = new JPanel();
 		roomPanel.setSize(180, 300);
-		JTextField roomInHand = new JTextField();
-		JTextField roomSeen = new JTextField();
+		roomInHand.setEditable(false);
 		roomPanel.setLayout(new GridLayout(2,1));
 		roomPanel.add(roomInHand, BorderLayout.CENTER);
 		roomPanel.add(roomSeen, BorderLayout.CENTER);
 		roomPanel.setBorder(BorderFactory.createTitledBorder("Known Rooms"));
 		
-		JPanel weaponPanel = new JPanel();
 		weaponPanel.setSize(180, 300);
-		JTextField weaponInHand = new JTextField();
-		JTextField weaponSeen = new JTextField();
 		weaponPanel.setLayout(new GridLayout(2,1));
 		weaponPanel.add(weaponInHand, BorderLayout.CENTER);
 		weaponPanel.add(weaponSeen, BorderLayout.CENTER);
 		weaponPanel.setBorder(BorderFactory.createTitledBorder("Known Weapons"));
 
-		
-		JLabel cardLabel = new JLabel();
-		JPanel cardPanel = new JPanel();
 		
 		cardLabel.setText("Known Cards");		
 		cardPanel.setLayout(new GridLayout(3, 1));
