5e9e8b91577f186ee839596482d5a16d8303c7d4
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-11-25 12:13:20 -0700

    added elements to accusation panel

---
+++ ClueGame/src/clueGame/GameAccusationPanel.java
@@ -0,0 +1,51 @@
+package clueGame;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class GameAccusationPanel extends JDialog {
+	
+	public GameAccusationPanel() {
+		setTitle("Make an Accusation");
+		setSize(350,150);
+		setLayout(new GridLayout(4,2));
+		
+		JLabel roomLabel = new JLabel("Current Room:");
+		JLabel personLabel = new JLabel("Person:");
+		JLabel weaponLabel = new JLabel("Weapon:");
+		
+		String[] choices = { "CHOICE 1","CHOICE 2", "CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6"};
+		
+		JTextField roomText = new JTextField("example Room");
+		roomText.setEditable(false);
+		JComboBox<String> personText = new JComboBox<String>(choices);
+		JComboBox<String> weaponText = new JComboBox<String>(choices);
+		
+		JButton submit = new JButton("Submit");
+		submit.addActionListener(weaponText);
+		JButton cancel = new JButton("cancel");
+
+		add(roomLabel);
+		add(roomText);
+		add(personLabel);
+		add(personText);
+		add(weaponLabel);
+		add(weaponText);
+		add(submit);
+		add(cancel);
+	
+		
+	}
+	
+	public static void main(String[] args) {
+		JDialog box = new GameAccusationPanel();
+		box.setVisible(true);
+	}
+
+}
