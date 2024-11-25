bee1b0c244cb7ccaa51e69cd5298e439b30af356
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-11-25 12:21:50 -0700

    added cancel button listener

---
+++ ClueGame/src/clueGame/ActionListenerAccusationSubmit.java
@@ -0,0 +1,12 @@
+package clueGame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActionListenerAccusationSubmit implements ActionListener {
+	
+	public void actionPerformed(ActionEvent e) {
+		// implement board accusation handling
+	}
+
+}
